a509c89729f61914088d9fbe151e2b470f00c7c3
Fortesa Krasniqi <73967363+fortesakrasniqi@users.noreply.github.com>
2022-04-08 12:03:52 +0200

    Create footer.php

---
+++ includes/footer.php
@@ -0,0 +1,21 @@
+
+        <img src="./images/peace.png">
+        <div class="footeri">
+            <div class="information">
+                <h2>INFORMATION</h2>
+                <p><a href="./faqs.php">FAQs</a></p>
+                <p>Privacy Policy</p>
+            </div>
+            <div class="contact">
+                <h2>CONTACT</h2>
+                <h3>964-502-3820</h3>
+                <p>mindpeace@gmail.com</p>
+                <p>3 Columbus Circle- 15/F, <b> New York </b>, NY 10019</p>
+            </div>
+            <div class="services">
+                <h2>THERAPY SERVICES</h2>
+                <p><a href="./services.php">Eating Disorders</a></p>
+                <p><a href="./services.php">Couples Therapy</a></p>
+                <p><a href="./services.php">Anxiety & Depression</a></p>
+            </div>
+        </div>
