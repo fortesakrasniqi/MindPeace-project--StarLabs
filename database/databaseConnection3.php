96cc9f85be55926a5a15ab60a895930973442fba
Fortesa Krasniqi <73967363+fortesakrasniqi@users.noreply.github.com>
2022-04-08 12:02:55 +0200

    Add files via upload

---
+++ database/databaseConnection3.php
@@ -0,0 +1,22 @@
+<?php
+
+class DBConnection3{
+	private $server="localhost";
+	private $username="root";
+	private $password="";
+	private $database="mindpeace1";
+
+
+	function startConnection(){
+		try{
+			$conn = new PDO("mysql:host=$this->server;dbname=$this->database;",$this->username,$this->password);
+			$conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+			return $conn;
+
+		}catch(PDOException $e){
+			echo "Connection Failed ".$e.getMessage();
+			return null;
+		}
+	}
+}
+?>
